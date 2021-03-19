d14bb3a0b981bb7fb6e9be2946a1e22b9386b693
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2021-03-20 00:57:42 +0530

    Updates

---
+++ precache-manifest.06dd66d76ffbab7fe136b1a5f6706230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0847b93eca6319ac1bf7409425f5d43b",
+    "url": "/Our-Menu/index.html"
+  },
+  {
+    "revision": "d77ec610001a408e1c10",
+    "url": "/Our-Menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Our-Menu/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Our-Menu/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77ec610001a408e1c10",
+    "url": "/Our-Menu/static/js/main.653cf612.chunk.js"
+  },
+  {
+    "revision": "8841c06aea43f180a7d6",
+    "url": "/Our-Menu/static/js/runtime-main.1e0eb7bc.js"
+  }
+]);
